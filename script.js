81e38acda6d9f722ec5cc7009e32fdc515f34e75
zainELd33n <abdulrahmanzain32@gmail.com>
2023-01-14 14:03:49 +0200

    Updated Pseudocode

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // create an input for the user to enter the answer
 // solve the case-sensitve answer by lower cassing the answer
+// create a random selector to choose the answer
+// create a funtcion that compares the users answer and the generated answer
+// show both answers
+// if the answer match return the statment "It's a draw !"
+// if the answers are different return a unique answer for each solution
