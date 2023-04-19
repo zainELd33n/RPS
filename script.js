004f58825bcea5d04662caacf7879f3f7723aa66
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-04-19 17:22:56 +0200

    added declarations

---
+++ script.js
@@ -0,0 +1,7 @@
+//Declarations 
+let playerScore = 0;
+let compScore = 0;
+const items = ['rock', 'paper', 'scissors'];
+const compChoice = items[Math.floor(Math.random()*items.length)];
+
+//Comparing system
