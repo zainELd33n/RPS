a3338a43f9668e2520a880516cedd67748cf7e03
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-04-22 22:04:49 +0200

    exchanged old tags for new

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //Declarations 
-let buttons = document.querySelector(".btns")
+let inputs = document.querySelector(".inputs")
 let playerScore = 0;
 let compScore = 0;
 const items = ['rock', 'paper', 'scissors'];
 const compChoice = items[Math.floor(Math.random()*items.length)];
-let playerChoice = buttons.addEventListener('pointerdown', (e) => {
-        playerChoice = e.target.alt
+let playerChoice = inputs.addEventListener('pointerdown', (e) => {
+        const target = e.target;
+        const choice = target.dataset.item;
+        console.log(choice)
     });
 
-
 //Comparing system
+    
 
 
 //Testing area
