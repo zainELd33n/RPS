cbfc4f42fff6afd199d57fa338cca27c83dab13c
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-04-22 21:17:49 +0200

    added playerChoice listener

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //Declarations 
+let buttons = document.querySelector(".btns")
 let playerScore = 0;
 let compScore = 0;
 const items = ['rock', 'paper', 'scissors'];
 const compChoice = items[Math.floor(Math.random()*items.length)];
+let playerChoice = buttons.addEventListener('pointerdown', (e) => {
+        playerChoice = e.target.alt
+    });
+
 
 //Comparing system
+
+
+//Testing area
+
+
