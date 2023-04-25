66086fd97dca233003971641fbca780b8f8f1969
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-04-25 10:22:43 +0200

    Finalized gameLogic function

--- script.js
+++ script.js
@@ -3,17 +3,37 @@ let inputs = document.querySelector(".inputs")
 let playerScore = 0;
 let compScore = 0;
 const items = ['rock', 'paper', 'scissors'];
-const compChoice = items[Math.floor(Math.random()*items.length)];
+
+const compChoice = inputs.addEventListener('pointerdown', () => {
+    items[Math.floor(Math.random()*items.length)];
+});
+
 let playerChoice = inputs.addEventListener('pointerdown', (e) => {
         const target = e.target;
         const choice = target.dataset.item;
-        console.log(choice)
+        console.log(` choice: ${choice}, compChoice ${compChoice}`);
+        //To make sure the logic works ^
+        let z = gameLogic(choice, compChoice);
+        console.log(z);
     });
 
 //Comparing system
-    
+ function gameLogic(a,b) {
+    if(a === 'rock' && b === 'rock' || a === 'paper' && b === 'paper' || a === 'scissors' && b === 'scissors') {
+        return 'Draw, No points'
+    }
+    else if (a === 'rock' && b === 'paper' || a === 'rock' && b === 'scissors' || a === 'rock' && b === 'scissors') {
+        return 'Player Won !'
+    }
+
+ }
 
 
 //Testing area
 
-
+/*
+if(playerChoice === 'rock' && compChoice === 'rock') {
+    console.log('true')
+    console.log(`${playerChoice}, ${compChoice}`)
+}
+*/
