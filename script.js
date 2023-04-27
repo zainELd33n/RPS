437cb4a85f43de0b8cfe1874be3b3d2a33db7da8
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-04-27 22:34:29 +0200

    commented for easier reading

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-//Declarations 
+//Declarations
 let inputs = document.querySelector(".inputs");
-let count = document.querySelector("#count")
+let count = 0;
 let playerScore = 0;
 let compScore = 0;
 const items = ['rock', 'paper', 'scissors'];
 
+//Computer's randomized choice
 let compChoice;
 inputs.addEventListener('pointerdown', () => {
     compChoice = items[Math.floor(Math.random()*items.length)];
 });
 
+
+//Listener for the input of the player
 let playerChoice = inputs.addEventListener('pointerdown', (e) => {
         const target = e.target;
         const choice = target.dataset.item;
@@ -17,9 +20,9 @@ let playerChoice = inputs.addEventListener('pointerdown', (e) => {
         //^ To make sure the logic works
         let z = gameLogic(choice, compChoice);
         console.log(z);
-    });
+});
 
-//Comparing system
+//Comparing logic
  function gameLogic(a,b) {
     if(a === 'rock' && b === 'rock' || a === 'paper' && b === 'paper' || a === 'scissors' && b === 'scissors') {
         return 'Draw, No points';
@@ -27,7 +30,7 @@ let playerChoice = inputs.addEventListener('pointerdown', (e) => {
     else if (a === 'rock' && b === 'scissors') {
         playerScore++;
         return 'Player Won !';
-    } 
+    }
     else if (a === 'paper' && b === 'rock') {
         playerScore++;
         return 'Player Won !';
@@ -45,9 +48,4 @@ let playerChoice = inputs.addEventListener('pointerdown', (e) => {
 
 //Testing area
 
-/*
-if(playerChoice === 'rock' && compChoice === 'rock') {
-    console.log('true')
-    console.log(`${playerChoice}, ${compChoice}`)
-}
-*/
+
